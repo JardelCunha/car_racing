4e55826b5f935e53c78f588c66abee07f2fe7a99
JardelCunha <jardelk2@gmail.com>
2022-06-17 06:07:34 -0300

    alterando a funcao de repawn dos carros

--- car_racing.py
+++ car_racing.py
@@ -44,9 +44,15 @@ def loop_game(inicio_estrada=estrada.inicio_estrada, final_estrada=estrada.final
     pos_jogador_y = altura - carros.altura_carro
 
     # posicao inicial carro inimigo para os 4 carros
+    '''
     pos_inimigo_x = [random.randint(inicio_estrada, final_estrada), random.randint(inicio_estrada, final_estrada),
                      random.randint(inicio_estrada, final_estrada), random.randint(inicio_estrada, final_estrada)]
+    '''
+    pos_inimigo_x = [respawn()[0], respawn()[1], respawn()[2], respawn()[3]]
     pos_inimigo_y = -100
+
+
+
 
     # relogio
     relogio = pygame.time.Clock()  # Frame
@@ -90,20 +96,27 @@ def loop_game(inicio_estrada=estrada.inicio_estrada, final_estrada=estrada.final
         # controle da animação da velocidade da estrada (ilusao de que o carro estará mais rápido)
         if score < 50:
             relogio.tick(20)
-        if 50 <= score < 100:
+        elif 50 <= score < 100:
             relogio.tick(30)
-        if 100 <= score < 150:
+        elif 100 <= score < 150:
             relogio.tick(32)
-        if 150 <= score < 200:
+        elif 150 <= score < 200:
             relogio.tick(34)
-        if 200 <= score < 250:
+        elif 200 <= score < 250:
             relogio.tick(33)
-        if 250 <= score < 300:
-            relogio.tick(38)
+        elif 250 <= score < 300:
+            relogio.tick(34)
+        elif 300 <= score < 350:
+            relogio.tick(35)
+        elif 350 <= score < 400:
+            relogio.tick(36)
+        elif 350 <= score < 400:
+            relogio.tick(36)
 
         # condicao de respawn
         if pos_inimigo_y > 719:
-            pos_inimigo_x = [respawn()[0], respawn()[1], respawn()[2], respawn()[3]]
+            posicoes_respawn = respawn()
+            pos_inimigo_x = [posicoes_respawn[0], posicoes_respawn[1], posicoes_respawn[2], posicoes_respawn[3]]
             pos_inimigo_y = -100
 
         # retangulos colisao
@@ -139,7 +152,7 @@ def loop_game(inicio_estrada=estrada.inicio_estrada, final_estrada=estrada.final
                             pygame.quit()
                             exit()
 
-                        ret_texto.center = (largura // 2, altura // 2)
+                        ret_texto.center = (largura // 2, 380)
                         ret_texto_2.center = (largura // 2, 460)
                         tela.blit(text_formatado_2, ret_texto_2)
                         tela.blit(text_formatado, ret_texto)
@@ -164,14 +177,31 @@ def loop_game(inicio_estrada=estrada.inicio_estrada, final_estrada=estrada.final
         texto_formatado = fonte.render(pontuacao, True, (255, 255, 255))
         tela.blit(texto_formatado, (65, 20))
         pygame.display.update()
+
     pygame.mixer.init()
 
 
-def respawn(inicio_estrada=estrada.inicio_estrada, final_estrada=estrada.final_estrada):
-    x_carro_inimigo = [random.randint(inicio_estrada, final_estrada),
-                       random.randint(inicio_estrada, final_estrada),
-                       random.randint(inicio_estrada, final_estrada),
-                       random.randint(inicio_estrada, final_estrada)]
+def respawn(inicio_estrada=estrada.inicio_estrada, final_estrada=estrada.final_estrada, largura_carro= carros.largura_carro):
+
+    lista_posicoes = [inicio_estrada,
+                      inicio_estrada+(largura_carro*1),
+                      inicio_estrada+(largura_carro*2),
+                      inicio_estrada+(largura_carro*3),
+                      inicio_estrada+(largura_carro*4),
+                      inicio_estrada+(largura_carro*5),
+                      inicio_estrada+(largura_carro*1),
+                      inicio_estrada+(largura_carro*6),
+                      inicio_estrada+(largura_carro*7),
+                      inicio_estrada+(largura_carro*8),
+                      inicio_estrada+(largura_carro*9),
+                      inicio_estrada+(largura_carro*10)]
+
+    #embaralha a lista de posicoes
+    random.shuffle(lista_posicoes)
+
+    #obtendo 4 elementos aleatorios
+    x_carro_inimigo = random.sample(lista_posicoes, 4)
+
     return x_carro_inimigo
 
 
